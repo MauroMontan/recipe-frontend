a78435a3cd47e757d97acc2310823ef35e156c6b
MauroMontan <mauromontano@outlook.es>
2021-08-23 16:50:54 -0500

    overlay color removed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,29 @@
 <template>
-  <v-container grid-list-md text-xs-center app fluid>
+  <v-container  grid-list-md text-xs-center app fluid>
     <v-img
       max-width="400"
       style="position: fixed; bottom: 55px; width: 700px; transform: scaleX(-1)"
       src="../assets/undraw_Hamburger_8ge6.svg"
     />
-    <h1
-      class="mt-2 mb-2"
-      align="center"
-      style="border-radius: 15px; color:#ff0059"
-    >
-      Enjoy cooking!
-    </h1>
+
     <v-layout row wrap align-center>
       <v-flex xs12 md12>
         <v-row style="display: flex" dense>
+          <v-col cols="12" md="12" xs="12" sm="12">
+            <h1
+             
+              align="center"
+              style="border-radius: 15px; color: #ff0059"
+            >
+              Enjoy cooking!
+            </h1>
+          </v-col>
           <v-col cols="12" md="6" xs="12" sm="12">
             <v-card
               style="border-radius: 15px"
               class="pa-5 mb-4 mt-2 mx-10"
               color="rgba(39, 39, 39, 0.8)"
               dark
-             
               elevation="20"
             >
               <h2 align="center">Store your own recipes</h2>
@@ -34,7 +36,7 @@
                   display: block;
                   margin-left: auto;
                   margin-right: auto;
-                  margin-top:20px;
+                  margin-top: 20px;
                   width: 60%;
                 "
                 src="../assets/undraw_cooking_lyxy.svg"
@@ -45,9 +47,8 @@
             <v-card
               @click="sayHi"
               style="border-radius: 15px"
-              class="pa-5 mb-4 mt-10 mx-10"
+              class="pa-5 mb-4 mt-2 mx-10"
               dark
-             
               color="rgba(39, 39, 39, 0.8)"
               elevation="12"
             >
@@ -62,7 +63,7 @@
                   display: block;
                   margin-left: auto;
                   margin-right: auto;
-                  margin-top:20px;
+                  margin-top: 20px;
                   width: 50%;
                 "
                 src="../assets/undraw_Add_files_re_v09g.svg"
